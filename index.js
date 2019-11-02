1c11eabce873a1f8fc7d62f76d43427d41951432
Taahira Carter <taahirac001@gmail.com>
2019-11-02 17:44:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if(string === string.toLowerCase){
+  var lowerCase = string.toLowerCase()
+  if(string === lowerCase{
     return "I can't hear you!"
   }
 }
